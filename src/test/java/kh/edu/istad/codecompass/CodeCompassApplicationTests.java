10ddf9f314bc55e8edd94423fabd75db0332f364
Panharoth06 <panharothchheng0903@gmail.com>
2025-08-05 21:02:30 +0700

    init

---
+++ src/test/java/kh/edu/istad/codecompass/CodeCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.istad.codecompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCompassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
