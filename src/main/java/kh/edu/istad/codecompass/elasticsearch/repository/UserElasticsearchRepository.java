e535bf476cf9b4c6551bb9b750d32f5dc4c9e25d
chengdevith <chengdevith5@gmail.com>
2025-08-20 17:57:00 +0700

    feat(Elasticsearch): testing with Elasticsearch

---
+++ src/main/java/kh/edu/istad/codecompass/elasticsearch/repository/UserElasticsearchRepository.java
@@ -0,0 +1,16 @@
+package kh.edu.istad.codecompass.elasticsearch.repository;
+
+import kh.edu.istad.codecompass.elasticsearch.domain.UserIndex;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface UserElasticsearchRepository extends ElasticsearchRepository<UserIndex,String> {
+
+    List<UserIndex> findByUsernameContaining(String keyword);
+
+    List<UserIndex> findByLevel(String level);
+
+    List<UserIndex> findByRank(Integer rank);
+
+}
