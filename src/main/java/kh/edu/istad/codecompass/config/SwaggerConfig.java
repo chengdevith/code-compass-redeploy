3316af0091920981490c07ba423f5ffc60043c5f
chengdevith <chengdevith5@gmail.com>
2025-09-03 17:59:48 +0700

    feat(Elasticsearch): change title,version,description

---
+++ src/main/java/kh/edu/istad/codecompass/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package kh.edu.istad.codecompass.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Code Compass",
+                version = "V1",
+                description = "description"
+        )
+)
+public class SwaggerConfig {
+}
