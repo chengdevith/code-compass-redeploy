b7c20ee83a5d9e0bf35ac88ce9e7b2f82ff3e894
Cheng Devith <cheng.devith.1222@rupp.edu.kh>
2025-09-03 18:30:35 +0700

    deploy

--- src/main/java/kh/edu/istad/codecompass/config/SwaggerConfig.java
+++ src/main/java/kh/edu/istad/codecompass/config/SwaggerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "Code Compass",
                 version = "V1",
-                description = "description"
+                description = "CodeCompass is an innovative online platform designed for coders of all skill levels to sharpen their programming skills and tackle coding challenges similar to LeetCode. With a user-friendly interface, CodeCompass offers a diverse range of coding tasks across various programming languages, allowing users to solve problems, test their solutions, and improve their algorithmic thinking. Whether you're preparing for technical interviews or simply looking to enhance your coding expertise, CodeCompass provides a dynamic and engaging environment to practice, learn, and grow."
         )
 )
 public class SwaggerConfig {
