862bb824b77b88847abaa4870c15fec1b857f1d9
Panharoth06 <panharothchheng0903@gmail.com>
2025-09-05 19:56:37 +0700

    Revert "feat(Swagger): allow origins for swagger"
    This reverts commit 63d5524485d2eda0400136db9e5c5f4f1c893f3f.

--- src/main/java/kh/edu/istad/codecompass/config/WebConfig.java
+++ src/main/java/kh/edu/istad/codecompass/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://code-compass.devith.it.com"
-                ) // Next.js dev server
+                .allowedOrigins("http://localhost:3000") // Next.js dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
