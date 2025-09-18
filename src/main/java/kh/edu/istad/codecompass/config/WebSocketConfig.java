80e5da046e83419f76240b45adde9b6ca9fbc0b3
chengdevith <chengdevith5@gmail.com>
2025-09-18 17:06:19 +0700

    feat(comment): fix WS config

--- src/main/java/kh/edu/istad/codecompass/config/WebSocketConfig.java
+++ src/main/java/kh/edu/istad/codecompass/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws-comments")
+        registry.addEndpoint("/api/v1/ws-comments")
                 .setAllowedOrigins("http://localhost:3000",
                         "https://code-compass.devith.it.com")
                 .withSockJS();
