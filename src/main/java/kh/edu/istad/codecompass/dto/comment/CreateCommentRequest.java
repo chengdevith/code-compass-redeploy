402c63c76212e5f5ee6ccbc3eaa8386f95ac7637
chengdevith <chengdevith5@gmail.com>
2025-09-25 18:12:40 +0700

    feat(cmt): change request body from userId to username

--- src/main/java/kh/edu/istad/codecompass/dto/comment/CreateCommentRequest.java
+++ src/main/java/kh/edu/istad/codecompass/dto/comment/CreateCommentRequest.java
@@ -3,7 +3,7 @@ package kh.edu.istad.codecompass.dto.comment;
 public record CreateCommentRequest(
 
         String comment,
-        Long userId,
+        String username,
         Long problemId
 
 ) {
