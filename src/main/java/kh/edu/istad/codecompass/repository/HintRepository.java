70e9b9ba87ea67828aae87ce715993bf2183a1a9
chengdevith <chengdevith5@gmail.com>
2025-09-26 16:38:43 +0700

    feat(cmt): get report

--- src/main/java/kh/edu/istad/codecompass/repository/HintRepository.java
+++ src/main/java/kh/edu/istad/codecompass/repository/HintRepository.java
@@ -1,12 +1,11 @@
 package kh.edu.istad.codecompass.repository;
 
 import kh.edu.istad.codecompass.domain.Hint;
-import kh.edu.istad.codecompass.domain.Problem;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface HintRepository extends CrudRepository<Hint, Long> {
+public interface HintRepository extends JpaRepository<Hint, Long> {
 
     List<Hint> findByProblem_Id(long problemId);
 
--- src/main/java/kh/edu/istad/codecompass/service/ReportService.java
+++ src/main/java/kh/edu/istad/codecompass/service/ReportService.java
@@ -4,10 +4,14 @@ import kh.edu.istad.codecompass.dto.report.ChangeStatusRequest;
 import kh.edu.istad.codecompass.dto.report.CreateReportRequest;
 import kh.edu.istad.codecompass.dto.report.ReportResponse;
 
+import java.util.List;
+
 public interface ReportService {
 
     ReportResponse createReport (CreateReportRequest request);
 
     void changeStatus (ChangeStatusRequest request);
+
+    List<ReportResponse> getReport();
 
 }
