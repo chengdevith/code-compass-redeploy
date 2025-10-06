86f8c0e0bf23980af955f7e6819454c26be9b780
Panharoth06 <panharothchheng0903@gmail.com>
2025-10-06 23:46:34 +0700

    fixed: leaderboard service

--- src/main/java/kh/edu/istad/codecompass/controller/LeaderboardController.java
+++ src/main/java/kh/edu/istad/codecompass/controller/LeaderboardController.java
@@ -2,9 +2,11 @@ package kh.edu.istad.codecompass.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import kh.edu.istad.codecompass.dto.leaderboard.LeaderboardPublicResponse;
 import kh.edu.istad.codecompass.dto.leaderboard.LeaderboardResponse;
 import kh.edu.istad.codecompass.service.LeaderBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,13 @@ public class LeaderboardController {
     public LeaderboardResponse getLeaderboard(@AuthenticationPrincipal Jwt jwt) {
         String username = jwt.getClaim("preferred_username");
         return leaderBoardService.getLeaderboardWithUser(username);
+    }
+
+    @GetMapping
+    @Operation(summary = "View public leader board (public)")
+    @PreAuthorize("permitAll()")
+    public LeaderboardPublicResponse getLeaderboardPublic() {
+        return leaderBoardService.getLeaderboardPublic();
     }
 
 }
